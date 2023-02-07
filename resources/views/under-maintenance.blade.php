eaf25fe4bce70a45672bc5dc61b078f5753d4f64
irfan907 <75222952+irfan907@users.noreply.github.com>
2023-02-08 02:00:00 +0500

    Added under-construction

---
+++ resources/views/under-maintenance.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Startup - Startup Website Template</title>
+
+    <link rel="stylesheet" href="{{ asset('under-c.css') }}">
+</head>
+
+<body>
+
+    <div class="overlay"></div>
+    <div class="stars" aria-hidden="true"></div>
+    <div class="starts2" aria-hidden="true"></div>
+    <div class="stars3" aria-hidden="true"></div>
+    <main class="main">
+        <section class="contact">
+            <h1 class="title">Awesome Thing</h1>
+            <h2 class="sub-title">Site Under Construction</h2>
+        </section>
+    </main>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
-})->name('home');
+    return view('under-maintenance');
+})->name('under');
 
-Route::view('about-us','about-us')->name('about_us');
-Route::view('contact-us','contact-us')->name('contact_us');
-Route::view('services','services')->name('services');
-Route::view('blogs','blogs.blogs-list')->name('blogs');
-Route::view('blogs/detail','blogs.blog-detail')->name('blogs_detail');
-Route::view('our-team','our-team')->name('our_team');
-Route::view('pricing','pricing')->name('pricing');
-Route::view('testimonials','testimonials')->name('testimonials');
-Route::view('get-a-quote','get-quote')->name('get_a_quote');
+
+Route::prefix('staging')->group(function () {
+    Route::get('/', function () {
+        return view('home');
+    })->name('home');
+    Route::view('about-us','about-us')->name('about_us');
+    Route::view('contact-us','contact-us')->name('contact_us');
+    Route::view('services','services')->name('services');
+    Route::view('blogs','blogs.blogs-list')->name('blogs');
+    Route::view('blogs/detail','blogs.blog-detail')->name('blogs_detail');
+    Route::view('our-team','our-team')->name('our_team');
+    Route::view('pricing','pricing')->name('pricing');
+    Route::view('testimonials','testimonials')->name('testimonials');
+    Route::view('get-a-quote','get-quote')->name('get_a_quote');
+});
+
+
