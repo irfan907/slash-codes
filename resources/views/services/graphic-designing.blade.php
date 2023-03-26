4494d4ffcf7275f792fa47ae70a59d2b705b3444
irfan907 <75222952+irfan907@users.noreply.github.com>
2023-03-27 02:33:31 +0500

    added graphic design c

--- resources/views/services/graphic-designing.blade.php
+++ resources/views/services/graphic-designing.blade.php
@@ -15,4 +15,89 @@
         </div>
     </div>
 </div> 
+<div class="container">
+    <div class="content-box">
+        <h2 class="text-center">Unleash the Power of Creativity with Slash Codes Graphic Design Services!
+        </h2>
+        <p>
+            Transform your brand's image with Slash Codes Graphic Design Services. From logo design to web graphics, our bespoke solutions are tailored to meet your unique requirements. Elevate your brand and stand out in a crowded market with Slash Codes.
+        </p>
+
+        <div class="bg-light my-5 p-4 rounded-2 row">
+            <h2 class="text-center">
+                Creative Ideas, Designing Dreams
+
+            </h2>
+            <div class="col-md-6">
+                <p>
+                    At Slash Codes, we offer a wide range of graphic design services that are customized to meet the unique needs of each client. Our team of expert designers specializes in creating captivating designs that leave a lasting impression. We believe that great design is essential to building a strong brand identity and setting your business apart from the competition.
+                    <br><br>
+                    Our graphic design services include everything from logo design and branding to web design and print collateral. We work closely with our clients to understand their goals, target audience, and unique brand personality. Our bespoke designs are crafted with precision and care, ensuring that every detail is perfect and every element is cohesive.
+                </p>
+            </div>
+            <div class="col-md-6">
+                <p>
+                    At Slash Codes, we understand the importance of delivering high-quality designs that meet our clients' expectations. Our designers have years of expertise and skills to create designs that are not only aesthetically pleasing but also effective in communicating the desired message. We’re committed to delivering exceptional work that exceeds our clients' expectations.
+                    <br><br>
+                    If you're looking for professional graphic design services that will help your business stand out in a crowded market, look no further than Slash Codes. Contact us today to learn more about how we can help elevate your brand image with our graphic design services.
+                </p>
+            </div>
+        </div>
+        <div class="my-5"></div>
+
+
+        <div class="row my-4">
+            <div class="col-md-6">
+                <h3>Customized Solutions
+                </h3>
+                <p>Our services are tailored to meet the unique needs of each client, ensuring that every design is personalized to their specific requirements.
+                </p>
+            </div>
+            <div class="col-md-6">
+                <img src="{{ asset('theme/img/carousel-1.jpg') }}" style="height:150px; width: 100%; object-fit: contain;" alt="">
+            </div>
+        </div>
+
+        <div class="row my-4">
+            <div class="col-md-6">
+                <img src="{{ asset('theme/img/carousel-1.jpg') }}" style="height:150px; width: 100%; object-fit: contain;" alt="">
+            </div>
+            <div class="col-md-6">
+                <h3>User-Centered Approach
+                </h3>
+                <p>We take a user-centered approach to design, prioritizing the user experience and creating designs that are intuitive and user-friendly.
+                </p>
+                </div>
+        </div>
+        <div class="row my-4">
+            <div class="col-md-6">
+                <h3>Innovative Solutions
+                </h3>
+                <p>We constantly push creative boundaries and offer innovative design solutions that go beyond conventional design practices.
+                </p>            
+            </div>
+            <div class="col-md-6">
+                <img src="{{ asset('theme/img/carousel-1.jpg') }}" style="height:150px; width: 100%; object-fit: contain;" alt="">
+            </div>
+        </div>
+        <div class="row my-5">
+            <div class="bg-light col-12 p-3 rounded-2">
+                <p>
+                    <h2 class="text-center">                    Why Work With Slash Codes?
+                    </h2>
+                    <p>
+                        When it comes to graphic design services, Slash Codes stands out as the perfect partner for all your design needs. With a team of expert designers who are passionate about creating unique and innovative designs, Slash Codes offers customized solutions that are tailored to meet your specific requirements.
+                        <br> <br>
+                        Our user-centered approach prioritizes the user experience, ensuring that every design is intuitive and user-friendly. We understand the importance of brand consistency and are committed to creating designs that are in line with your brand identity.
+                        <br> <br>
+                        With Slash Codes, you can rest assured that your design needs are in the best hands, and we will go above and beyond to deliver exceptional designs that exceed your expectations.
+                        <br> <br>
+                        Let’s work together to create something exceptional for your audience.
+                    </p>
+
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
